54e27ffdf4e188d2714c3aa25caac072a7e72fd7
fartem <jaman.smlnsk@gmail.com>
2020-03-08 16:48:59 +0300

    2020-03-08 Update: Refactored MemoryShot class

--- lib/minfo/memory_shot.rb
+++ lib/minfo/memory_shot.rb
@@ -19,15 +19,19 @@ module Minfo
         free = 0
         File.open('/proc/meminfo').read.each_line { |line|
           if line.start_with?('MemTotal:')
-            total = line.split(/ +/)[1].to_i
+            total = extract_memory_value(line)
             @@memory_shot['total'] = MemoryUnit.new(total)
           elsif line.start_with?('MemFree:')
-            free = line.split(/ +/)[1].to_i
+            free = extract_memory_value(line)
             @@memory_shot['free'] = MemoryUnit.new(free)
           end
         }
         @@memory_shot['used'] = MemoryUnit.new(total - free)
       end
+    end
+
+    def extract_memory_value(proc_string)
+      proc_string.split(/ +/)[1].to_i
     end
 
     def free

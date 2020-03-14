eb5b393d9e6fb49c6a8cc0cf7260160b57097de9
fartem <jaman.smlnsk@gmail.com>
2020-03-14 22:44:23 +0300

    2020-03-14 Update: Updated comments

--- lib/minfo.rb
+++ lib/minfo.rb
@@ -4,6 +4,7 @@ module Minfo
   class PlatformError < StandardError; end
 
   # Class for executes memory shot.
+  # If OS is not Linux - raises PlatformError (because supports only Linux).
   class MemoryInfo
     def memory_shot
       return Minfo::MemoryShot.new if RUBY_PLATFORM.include?('linux')
